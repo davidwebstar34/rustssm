a654321a4b76ec4c1a9676931c33a8100f1fce5f
David <dwebster182@gmail.com>
2024-12-13 15:07:35 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-// main.rs
-
-use clap::{App, Arg};
+use clap::{Arg, Command};
 
 fn main() {
-    let matches = App::new("rustssm")
+    let matches = Command::new("rustssm")
         .version(env!("CARGO_PKG_VERSION"))
         .author("Your Name <your_email@example.com>")
         .about("Executes AWS SSM commands with an interactive CLI")
@@ -11,22 +9,22 @@ fn main() {
             Arg::new("exec")
                 .short('e')
                 .long("exec")
-                .takes_value(true)
-                .required(true)
-                .about("[required] Execute command"),
+                .value_name("EXEC")
+                .help("[required] Execute command")
+                .required(true),
         )
         .arg(
             Arg::new("target")
                 .short('t')
                 .long("target")
-                .takes_value(true)
-                .about("[optional] EC2 instanceId or name"),
+                .value_name("TARGET")
+                .help("[optional] EC2 instanceId or name"),
         )
         .get_matches();
 
     // Retrieve the "exec" and "target" arguments
-    let exec_command = matches.value_of("exec").unwrap();
-    let target = matches.value_of("target");
+    let exec_command = matches.get_one::<String>("exec").expect("required");
+    let target = matches.get_one::<String>("target");
 
     // Print the received arguments (Placeholder for the actual logic)
     println!("Executing command: {}", exec_command);
